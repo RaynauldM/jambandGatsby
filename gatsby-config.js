5b700025688bb85c1772b13a72d88fa9b70c1f6b
RaynauldM <raynauldminkema@gmail.com>
2023-02-27 20:57:05 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Helsdeur Jam App`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
